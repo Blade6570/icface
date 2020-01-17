0cbeb18da5eb7f6eacd316777fe750cac11981a3
Soumya Tripathy <st11@iitbbs.ac.in>
2020-01-17 17:20:45 +0200

    updated WAVC 2020 version

--- test_code_released_new/image_crop.py
+++ test_code_released_new/image_crop.py
@@ -7,14 +7,20 @@ import cv2
 import numpy as np
 from PIL import Image
 #import pdb   
-
+from imutils.face_utils import FaceAligner
 #def pre_process(path):
 for i in range(2):    
-    detector = dlib.get_frontal_face_detector()
-
-    cap = cv2.imread('/home/esa/Pictures/some.jpg') # add your image here
     
-    image= cv2.resize(cap, (400, 400))
+    predictor = dlib.shape_predictor('./shape_predictor_5_face_landmarks.dat')
+
+    fa = FaceAligner(predictor, desiredFaceWidth=256)
+
+  #  detector = dlib.get_frontal_face_detector()
+    detector = dlib.cnn_face_detection_model_v1('./mmod_human_face_detector.dat')
+
+    image = cv2.imread('/home/nvme/soumya/test_ICface2/new_crop/pd.jpg') # add your image here
+    
+    #image= cv2.resize(cap, (256, 256))
 
     RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
 
@@ -25,9 +31,16 @@ for i in range(2):
     rects = detector(RGB, 1)
     faces = dlib.full_object_detections()
 
+#    faceAligned = fa.align(RGB, gray, rects[0].rect)
+ #   rects_a = detector(faceAligned, 1)
+
     for rect in rects:
-                    c1=rect.dcenter()
-                    (x, y, w, h) = rect_to_bb(rect)
+                    c1=rect.rect.dcenter()
+                    #(x, y, w, h) = rect_to_bb(rect)
+                    x=rect.rect.left()
+                    y=rect.rect.top()
+                    w=rect.rect.right()-x
+                    h=rect.rect.bottom()-y
                     w=np.int(w*1.6) 
                     h=np.int(h*1.6) 
                     x=c1.x-np.int(w/2.0)
@@ -36,7 +49,9 @@ for i in range(2):
                        y=0
                     if x<0:
                        x=0
-                       
+
+                    
+   
                     faceOrig = imutils.resize(RGB[y:y+h, x:x+w],height=256) #y=10,h+60,W+40
   
                     d_num = np.asarray(faceOrig)
